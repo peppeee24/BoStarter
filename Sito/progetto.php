79c53ef8fc19ed62390c16c6f8023579421facee
feffo26 <65596046+feffo26@users.noreply.github.com>
2025-04-17 16:39:46 +0200

    Fixx finanziamento, max uno al giorno

--- Sito/progetto.php
+++ Sito/progetto.php
@@ -160,6 +160,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo htmlspecialchars($progetto['nome']); ?> - BoStarter</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
 
